6b367d7af72a0756b01b2533e30b9627f1e63831
Andrez23 <marincarlosandres42@gmail.com>
2023-07-19 20:49:13 -0500

    Creando parte del frontend (slider, navbar y grid)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Contactenos from './view/Contactenos';
+import Home from './view/Home';
+import Nosotros from './view/Nosotros';
+import Resena from './view/Resena';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+       <Navbar/>
+      <Routes>
+        <Route path='contactenos' element={<Contactenos/>}/>
+        <Route path='nosotros' element={<Nosotros/>}/>
+        <Route path='resena' element={<Resena/>}/>
+        <Route path='home' element={<Home/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        Correo:
+        Telefono:
+        Redes:
+        Direccion:
+    </div>
+  )
+}
+
+export default Footer
