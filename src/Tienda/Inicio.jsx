8b4372cbbbf5bbd484d7d8e47251ec4bd4230202
YayaT03 <dahianat45@outlook.es>
2023-08-13 10:19:22 -0500

    dieño tienda

--- src/Tienda/Inicio.jsx
+++ src/Tienda/Inicio.jsx
@@ -17,7 +17,7 @@ const Inicio = () => {
                 <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               </div>
               <div className="card-footer">
-                <small className="text-body-secondary">Last updated 3 mins ago</small>
+                <small><button className="btn btn-danger" type="submit">Ordenar</button></small>
               </div>
             </div>
           </div>
@@ -29,7 +29,7 @@ const Inicio = () => {
                 <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
               </div>
               <div className="card-footer">
-                <small className="text-body-secondary">Last updated 3 mins ago</small>
+                <small> <button className="btn btn-danger" type="submit">Ordenar</button></small>
               </div>
             </div>
           </div>
@@ -41,7 +41,7 @@ const Inicio = () => {
                 <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
               </div>
               <div className="card-footer">
-                <small className="text-body-secondary">Last updated 3 mins ago</small>
+                <small><button className="btn btn-danger" type="submit">Ordenar</button></small>
               </div>
             </div>
           </div>
